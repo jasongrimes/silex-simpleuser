c7207872af87744c9ef5299836eabd7261157d3e
Jason Grimes <jason@grimesit.com>
2013-04-15 08:15:53 +0000

    Add register and view actions.

--- src/SimpleUser/UserController.php
+++ src/SimpleUser/UserController.php
@@ -2,6 +2,7 @@
 
 namespace SimpleUser;
 
+use InvalidArgumentException;
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -41,5 +42,67 @@ class UserController
             'error' => $app['security.last_error']($request),
             'last_username' => $app['session']->get('_security.last_username'),
         ));
+    }
+
+    public function registerAction(Application $app, Request $request)
+    {
+        if ($request->isMethod('POST')) {
+            try {
+                $user = $this->createUserFromRequest($request);
+                $this->userManager->insert($user);
+                $app['session']->getFlashBag()->set('alert', 'Account created.');
+
+                return $app->redirect($app['url_generator']->generate('user.view', array('id' => $user->getId())));
+
+            } catch (InvalidArgumentException $e) {
+                $error = $e->getMessage();
+            }
+        }
+
+        return $app['twig']->render('@user/register.twig', array(
+            'layout_template' => $this->layoutTemplate,
+            'error' => isset($error) ? $error : null,
+            'name' => $request->request->get('name'),
+            'email' => $request->request->get('email'),
+        ));
+    }
+
+    /**
+     * @param Request $request
+     * @return User
+     * @throws InvalidArgumentException
+     */
+    protected function createUserFromRequest(Request $request)
+    {
+        if ($request->request->get('password') != $request->request->get('confirm_password')) {
+            throw new InvalidArgumentException('Passwords don\'t match.');
+        }
+
+        $user = $this->userManager->createUser(
+            $request->request->get('email'),
+            $request->request->get('password'),
+            $request->request->get('name') ?: null);
+
+        $errors = $user->validate();
+        if (!empty($errors)) {
+            throw new InvalidArgumentException(implode('<br>', $errors));
+        }
+
+        if ($this->userManager->findBy(array('email' => $user->getEmail()))) {
+            throw new InvalidArgumentException('An account with that email address already exists.');
+        }
+
+        return $user;
+    }
+
+    public function viewAction(Application $app, Request $request, $id)
+    {
+        $user = $this->userManager->getUser($id);
+
+        return $app['twig']->render('@user/view.twig', array(
+            'layout_template' => $this->layoutTemplate,
+            'user' => $user,
+        ));
+
     }
 }
