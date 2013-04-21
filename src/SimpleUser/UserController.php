f06287563dfcc4ffa4f2aa7bbc814cff2e713cc2
Jason Grimes <jason@grimesit.com>
2013-04-21 03:52:00 -0400

    Add edit, list, viewSelf actions, auto-login after registration.

--- src/SimpleUser/UserController.php
+++ src/SimpleUser/UserController.php
@@ -2,11 +2,19 @@
 
 namespace SimpleUser;
 
-use InvalidArgumentException;
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use InvalidArgumentException;
 
+/**
+ * Controller with actions for handling form-based authentication and user management.
+ *
+ * @package SimpleUser
+ */
 class UserController
 {
     /** @var UserManager */
@@ -14,6 +22,12 @@ class UserController
 
     protected $layoutTemplate = '@user/layout.twig';
 
+    /**
+     * Constructor.
+     *
+     * @param UserManager $userManager
+     * @param array $options
+     */
     public function __construct(UserManager $userManager, $options = array())
     {
         $this->userManager = $userManager;
@@ -23,6 +37,9 @@ class UserController
         }
     }
 
+    /**
+     * @param array $options
+     */
     public function setOptions(array $options)
     {
         if (array_key_exists('layout_template', $options)) {
@@ -30,11 +47,21 @@ class UserController
         }
     }
 
+    /**
+     * @param string $layoutTemplate
+     */
     public function setLayoutTemplate($layoutTemplate)
     {
         $this->layoutTemplate = $layoutTemplate;
     }
 
+    /**
+     * Login action.
+     *
+     * @param Application $app
+     * @param Request $request
+     * @return Response
+     */
     public function loginAction(Application $app, Request $request)
     {
         return $app['twig']->render('@user/login.twig', array(
@@ -44,6 +71,13 @@ class UserController
         ));
     }
 
+    /**
+     * Register action.
+     *
+     * @param Application $app
+     * @param Request $request
+     * @return Response
+     */
     public function registerAction(Application $app, Request $request)
     {
         if ($request->isMethod('POST')) {
@@ -51,6 +85,13 @@ class UserController
                 $user = $this->createUserFromRequest($request);
                 $this->userManager->insert($user);
                 $app['session']->getFlashBag()->set('alert', 'Account created.');
+
+                // Log the user in to the new account.
+                if (null !== ($current_token = $app['security']->getToken())) {
+                    $providerKey = method_exists($current_token, 'getProviderKey') ? $current_token->getProviderKey() : $current_token->getKey();
+                    $token = new UsernamePasswordToken($user, null, $providerKey);
+                    $app['security']->setToken($token);
+                }
 
                 return $app->redirect($app['url_generator']->generate('user.view', array('id' => $user->getId())));
 
@@ -83,21 +124,30 @@ class UserController
             $request->request->get('password'),
             $request->request->get('name') ?: null);
 
-        $errors = $user->validate();
+        $errors = $this->user_manager->validate();
         if (!empty($errors)) {
             throw new InvalidArgumentException(implode('<br>', $errors));
-        }
-
-        if ($this->userManager->findBy(array('email' => $user->getEmail()))) {
-            throw new InvalidArgumentException('An account with that email address already exists.');
         }
 
         return $user;
     }
 
+    /**
+     * View user action.
+     *
+     * @param Application $app
+     * @param Request $request
+     * @param int $id
+     * @return Response
+     * @throws NotFoundHttpException if no user is found with that ID.
+     */
     public function viewAction(Application $app, Request $request, $id)
     {
         $user = $this->userManager->getUser($id);
+
+        if (!$user) {
+            throw new NotFoundHttpException('No user was found with that ID.');
+        }
 
         return $app['twig']->render('@user/view.twig', array(
             'layout_template' => $this->layoutTemplate,
@@ -107,20 +157,118 @@ class UserController
 
     }
 
+    public function viewSelfAction(Application $app) {
+        if (!$app['user']) {
+            return $app->redirect($app['url_generator']->generate('user.login'));
+        }
+
+        return $app->redirect($app['url_generator']->generate('user.view', array('id' => $app['user']->getId())));
+    }
+
+    /**
+     * @param string $email
+     * @param int $size
+     * @return string
+     */
     protected function getGravatarUrl($email, $size = 80)
     {
         // See https://en.gravatar.com/site/implement/images/ for available options.
         return '//www.gravatar.com/avatar/' . md5(strtolower(trim($email))) . '?s=' . $size . '&d=identicon';
     }
 
+    /**
+     * Edit user action.
+     *
+     * @param Application $app
+     * @param Request $request
+     * @param int $id
+     * @return Response
+     * @throws NotFoundHttpException if no user is found with that ID.
+     */
     public function editAction(Application $app, Request $request, $id)
     {
+        $errors = array();
+
         $user = $this->userManager->getUser($id);
 
-        return $app['twig']->render('@user/view.twig', array(
+        if (!$user) {
+            throw new NotFoundHttpException('No user was found with that ID.');
+        }
+
+        if ($request->isMethod('POST')) {
+            $user->setName($request->request->get('name'));
+            $user->setEmail($request->request->get('email'));
+            if ($request->request->get('password')) {
+                if ($request->request->get('password') != $request->request->get('confirm_password')) {
+                    $errors['password'] = 'Passwords don\'t match.';
+                } else {
+                    $this->userManager->setUserPassword($user, $request->request->get('password'));
+                }
+            }
+            if ($app['security']->isGranted('ROLE_ADMIN') && $request->request->has('roles')) {
+                $user->setRoles($request->request->get('roles'));
+            }
+
+            $errors += $this->userManager->validate($user);
+
+            if (empty($errors)) {
+                $this->userManager->update($user);
+                $msg = 'Saved account information.' . ($request->request->get('password') ? ' Changed password.' : '');
+                $app['session']->getFlashBag()->set('alert', $msg);
+            }
+        }
+
+        return $app['twig']->render('@user/edit.twig', array(
             'layout_template' => $this->layoutTemplate,
+            'error' => implode('<br>', $errors),
             'user' => $user,
-            'imageUrl' => $this->getGravatarUrl($user->getEmail()),
+            'available_roles' => array('ROLE_USER', 'ROLE_ADMIN'),
+            'image_url' => $this->getGravatarUrl($user->getEmail()),
+        ));
+    }
+
+    public function listAction(Application $app, Request $request)
+    {
+        $limit = $request->get('limit') ?: 50;
+        $offset = $request->get('offset') ?: 0;
+        $order_by = $request->get('order_by') ?: 'id';
+        $order_dir = $request->get('order_dir') == 'DESC' ? 'DESC' : 'ASC';
+
+        $numResults = $this->userManager->findCount();
+
+        $users = $this->userManager->findBy(array(), array(
+            'limit' => array($offset, $limit),
+            'order_by' => array($order_by, $order_dir),
+        ));
+
+        foreach ($users as $user) {
+            $user->imageUrl = $this->getGravatarUrl($user->getEmail(), 40);
+        }
+
+        $nextUrl = $prevUrl = null;
+        if ($numResults > $limit) {
+            $nextOffset = ($offset + $limit) < $numResults  ? $offset + $limit : null;
+            $prevOffset = $offset > 0 ? (($offset - $limit) > 0 ? $offset - $limit : 0) : null;
+
+            $baseUrl = $app['url_generator']->generate('user.list') . '?limit=' . $limit . '&order_by=' . $order_by . '&order_dir=' . $order_dir;
+            if ($nextOffset !== null) {
+                $nextUrl = $baseUrl . '&offset=' . $nextOffset;
+            }
+            if ($prevOffset !== null) {
+                $prevUrl = $baseUrl . '&offset=' . $prevOffset;
+            }
+        }
+        $firstResult = $offset + 1;
+        $lastResult = ($offset + $limit) > $numResults ? $numResults : $offset + $limit;
+
+        return $app['twig']->render('@user/list.twig', array(
+            'layout_template' => $this->layoutTemplate,
+            'users' => $users,
+            'numResults' => $numResults,
+            'nextUrl' => $nextUrl,
+            'prevUrl' => $prevUrl,
+            'firstResult' => $firstResult,
+            'lastResult' => $lastResult,
         ));
 
     }
